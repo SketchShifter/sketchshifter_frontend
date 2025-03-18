a2410355c9cde199971916f06a7505af0d8ec8f8
ryohamag <Ryojune21@gmail.com>
2025-03-18 15:57:54 +0900

    特に無し

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { use, useEffect, useState } from 'react';
+import Card from '../components/Card';
+
 
 export default function HomePage() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState([] as any);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:8080/api/works');
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData();
+  }, []);
+
+  console.log(data);
+  console.log(data.works);
 
   return (
     <div>
@@ -31,6 +46,11 @@ export default function HomePage() {
           <Link href="/guests/preview">ゲストプレビュー</Link>
         </li>
       </ul>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.works && data.works.map((work) => (
+          <Card key={work.id} id={work.id} title={work.title} created_at={work.created_at} description={work.description} />
+        ))}
+      </div>
     </div>
   );
 }
