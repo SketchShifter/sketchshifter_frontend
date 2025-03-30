31891c8bc1ce3640c0a9a1a012ee9c01f5a5b55a
ryohamag <Ryojune21@gmail.com>
2025-03-30 17:48:03 +0900

    作品一覧画面を実装(内容はホーム画面と同一)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <header>
           <TopBar />
         </header>
-        <main className="bg-gray-100">
+        <main className="bg-gray-50">
           <ToastContainer />
           <div>
             {children}
